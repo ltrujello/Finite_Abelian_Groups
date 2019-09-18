7d494a905c62ee04b73a2cb32b88d80db3020a0e
ltrujello <ltrujillo@g.hmc.edu>
2019-09-18 15:04:47 -0700

    fixed bugs(?)

--- fin_ab_program.py
+++ fin_ab_program.py
@@ -6,41 +6,46 @@ def all_factorizations(theint):
 print("Finite abelian group of order: ")
 ord = input()
 try:
-   val = int(ord)
+   val = int(eval(ord))
 except ValueError:
    print("That's not an integer!")
 if val < 2:
     print("Pick something bigger.")
 else:
    the_factors = all_factorizations(val)
-   print(the_factors)
 
 def group_fac_for_latex(list_facts):
-   latex_code = ""
-   for fact in list_facts:
-      for i in range(len(fact)):
-        num = fact[i]
-        if i == 0:    
-            latex_code += "$\\mathbb{Z}_" + "{" + str(num) + "}" + "\\times "
-        elif i == len(fact) - 1:
-            latex_code += "\\mathbb{Z}_" + "{" + str(num) + "}$" + " \\newline"   
-        else: 
-            latex_code += "\\mathbb{Z}_" + "{" + str(num) + "}" + "\\times "
-      latex_code += "\n"
-   return latex_code
+    latex_code = ""
+    for fact in list_facts:
+        #latex_code += "$"
+        for i in range(len(fact)):
+            num = fact[i]
+            if i == 0:    
+                latex_code += "\\mathbb{Z}_" + "{" + str(num) + "}"
+            elif i == len(fact) - 1:
+                latex_code += "\\times \\mathbb{Z}_" + "{" + str(num) + "}"   
+            else: 
+                latex_code += "\\times \\mathbb{Z}_" + "{" + str(num) + "}" + "\\times "
+        if fact != list_facts[-1]:
+            latex_code += "\\\\"
+    return latex_code
 file = open('finite_abelian_factorization.tex', 'w')
 file.write("\\documentclass[12pt,letterpaper,boxed]{maths_v5}\n" 
-+ "\\usepackage[margin=1in]{geometry}\n" 
++ "\\usepackage[margin=0.3in]{geometry}\n" 
 + "\\usepackage{lmodern}\n" 
 + "\\usepackage{amsmath}\n"
++ "\\allowdisplaybreaks\n"
 + "\\begin{document}\n"
 + "\\begin{center}\n"
-+ "Finite Abelian Groups of Order :" + str(ord) + "\\newline \n"
-+ group_fac_for_latex(the_factors)
++ "Finite Abelian Groups of Order: " + str(val) + "\\newline \n"
 + "\\end{center}\n"
++ "\\begin{align}"
++ group_fac_for_latex(the_factors)
++ "\\end{align}"
 + "\\end{document}\n")
 file.close()
 
+print("All unique factorizations of " + str(val) + " are ", the_factors)
 os.system("latex finite_abelian_factorization.tex")
 os.system("pdflatex finite_abelian_factorization.tex")
 os.system("open finite_abelian_factorization.pdf")
